e9801a8f16b96b10f327d906b80345bb14e97bbf
Andrea Tarocchi <atarocch@redhat.com>
2018-05-11 19:51:46 +0200

    Initial commit.

---
+++ src/main/java/com/redhat/fuse/boosters/rest/http/IndexRootMapper.java
@@ -0,0 +1,21 @@
+package com.redhat.fuse.boosters.rest.http;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+public class IndexRootMapper extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/rest-http-index.html");
+    }
+
+    // @Override
+    // public void addViewControllers(ViewControllerRegistry registry) {
+    //     registry.addViewController("/").setStatusCode(HttpStatus.PERMANENT_REDIRECT).setViewName("redirect:/webjars/swagger-ui/index.html?url=/camel/api-doc&validatorUrl=");
+    // }
+}
